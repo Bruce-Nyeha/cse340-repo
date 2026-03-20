eb4e756021e6a4bb9c438b3f51d0f6d48e692355
Bruce-Nyeha <brucenyeha@gmail.com>
2026-03-20 09:29:26 +0000

    Week 03

--- server.js
+++ server.js
@@ -7,32 +7,38 @@
  *************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
+require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+
 
 /* ***********************
  * View Engine and Templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root 
+app.set("layout", "./layouts/layout")
+
+
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 
-// Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+// Index Route
+app.get("/", baseController.buildHome)
+
+app.use("/inv", inventoryRoute)
+
 
 /* ***********************
  * Local Server Information
- * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT 
+const host = process.env.HOST 
+
 
 /* ***********************
  * Log statement to confirm server operation
